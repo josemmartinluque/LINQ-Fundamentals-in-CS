30c88e2b8419db2cdbc7619a9ee8cb4bb62a79b5
josemmartinluque <jose.m.martin.luque@avanade.com>
2024-04-05 12:57:24 +0200

    Module 5 completed

--- 05-Where/folder/Program.cs
+++ 05-Where/folder/Program.cs
@@ -4,7 +4,7 @@
 SamplesViewModel vm = new();
 
 // Call Sample Method
-var result = vm.OrderByTwoFieldsDescMethod();
+var result = vm.WhereExtensionMethod();
 
 // Display Results
 vm.Display(result);
